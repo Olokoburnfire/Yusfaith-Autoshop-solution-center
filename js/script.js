785b2c5bea59bf03dca49fa2272b7948e7f2f28e
Olokodana AbdulKabir <Olokodanaabdulkabir@gmail.com>
2022-07-21 02:51:30 +0100

    Add files via upload
    Another version of the website

---
+++ js/script.js
@@ -0,0 +1,69 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //01D338AFB1EE90142DA813D91EA1A13D0BB4               
+  //onsubmit="sendEmail(); reset(); return false;"
